cc2abb867f24fa1d6fa394ffd9fe7f3b53235a03
Lee <jessy1092@gmail.com>
2016-11-10 02:26:10 +0800

    chore: Add gulpfile.babel.js to build and transpile

---
+++ gulpfile.babel.js
@@ -0,0 +1,20 @@
+
+import gulp       from 'gulp';
+import gbabel     from 'gulp-babel';
+
+const TRANSPILE_PATH = './lib';
+const SOURCE_PATH = './src';
+
+gulp.task('transpile', () => {
+  return gulp.src(`${SOURCE_PATH}/**/*`)
+    .pipe(gbabel({presets: ['es2015', 'react', 'stage-3']}))
+    .pipe(gulp.dest(TRANSPILE_PATH));
+});
+
+gulp.task('watch', () => {
+  gulp.watch(`${SOURCE_PATH}/**/*`, ['build']);
+});
+
+gulp.task('build', ['transpile']);
+gulp.task('dev', ['build', 'watch']);
+gulp.task('default', ['build']);
